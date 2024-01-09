ed236998d35d56c9ce8dfa0f352fe255abe4d80d
MansAlien <mans012033945421@gmail.com>
2024-01-09 22:49:58 +0200

    login, logout, signup, and custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
+    class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = ("username", "email", "age",)
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ("username", "email", "age",)
