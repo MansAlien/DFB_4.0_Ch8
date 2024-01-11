374865a1b0d59fc8dbf18a2f839a7a6aca9b517f
MansAlien <mans012033945421@gmail.com>
2024-01-11 15:12:46 +0200

    adding the change_reset password

--- project/settings.py
+++ project/settings.py
@@ -138,3 +138,10 @@ LOGOUT_REDIRECT_URL = 'home'
 #crispy settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mans012033945420@gmail.com'
+EMAIL_HOST_PASSWORD = 'erhsyvuhkpxkyjcb '
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
